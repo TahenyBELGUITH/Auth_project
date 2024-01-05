07ec9227f96a8a38bff34e7b48c505fe807c3fbb
TahenyBELGUITH <taheny.belguith@gmail.com>
2024-01-05 17:57:24 +0100

    updating...

--- src/ResetPass/ResetPass.jsx
+++ src/ResetPass/ResetPass.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
-import { Link } from "react-router-dom";
 
 const ResetPass = ({
   navigation,
